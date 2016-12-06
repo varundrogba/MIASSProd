9d26dcf306740cef25709ffa4b6c518090759ee0
Varun Shah <varundrogba@gmail.com>
2016-12-06 13:00:03 -0800

    UI + mandar changes

--- pages/controllers/sensors_controller.js
+++ pages/controllers/sensors_controller.js
@@ -1,7 +1,7 @@
 var sensor_controller=angular.module('sensor_controller',[])
 
 
-sensor_controller.controller('sensor_control',['$scope','$http',function($scope,$http){
+sensor_controller.controller('sensor_control',['$scope','$http','$window',function($scope,$http,$window){
 var user_email;
 
 	$http.get('/session_req_sensor_controller').success(function(response){
@@ -14,14 +14,15 @@ var user_email;
 
 		console.log(new Date());
 		$http.get('/session_req_sensor_controller').success(function(response){
+
 		console.log("inside");
 		console.log(response);
 		var date = new Date();
 		$http.post('/new_fitbit/'+response+'/'+date).success(function(response){
 			console.log(response);
 		});
-
-
+		
+		$window.alert("New Sensor Cluster Hired!");
 	});
 	}
 
@@ -29,14 +30,27 @@ var user_email;
 //-------------------------------- to remove fitbit from network
 
 	$scope.fitbit_end_date=function(){
-
-		console.log(new Date());
 		$http.get('/session_req_sensor_controller').success(function(response){
+
+		$http.get('/user_activeSensor_query/'+response).success(function(response1){
+			console.log(response1+"-----------"+response1[0].fitbit.length);
+		
+		if(response1[0].fitbit.length!=0){
+		console.log(new Date());
 		console.log("inside");
 		console.log(response);
 		var date = new Date();
 		$http.post('/stop_fitbit/'+response+'/'+date).success(function(response){
 			console.log(response);
+		});
+		$window.alert("Fitbit Sensor Deprovisioned");
+		}
+		else{
+
+			console.log("No fitbit to stop");
+			$window.alert("No Sensors Avaialable for Deprovision");
+		}
+
 		});
 
 
@@ -65,14 +79,26 @@ var user_email;
 
 		console.log(new Date());
 		$http.get('/session_req_sensor_controller').success(function(response){
+
+		$http.get('/user_activeSensor_query/'+response).success(function(response1){
+			console.log(response1+"-----------"+response1[0].fitbit.length);
+
+		if(response1[0].plane.length!=0){
 		console.log("inside");
 		console.log(response);
 		var date = new Date();
 		$http.post('/stop_plane/'+response+'/'+date).success(function(response){
 			console.log(response);
 		});
+		$window.alert("Plane Sensor Deprovisioned");
+		}
+		else{
 
+			console.log("No Plane to stop");
+			$window.alert("No Sensors Avaialable for Deprovision");
+		}			
 
+	});
 	});
 	}
 
@@ -97,17 +123,28 @@ var user_email;
 
 		console.log(new Date());
 		$http.get('/session_req_sensor_controller').success(function(response){
+
+		$http.get('/user_activeSensor_query/'+response).success(function(response1){
+			console.log(response1+"-----------"+response1[0].fitbit.length);
+		if(response1[0].car.length!=0){	
 		console.log("inside");
 		console.log(response);
 		var date = new Date();
 		$http.post('/stop_car/'+response+'/'+date).success(function(response){
 			console.log(response);
 		});
+			$window.alert("Car Sensor Deprovisioned");
+		}
+		else{
 
+				console.log("No Plane to stop");
+				$window.alert("No Sensors Avaialable for Deprovision");
+		}			
+
+		});
 
 	});
 	}
-
 
 
 
